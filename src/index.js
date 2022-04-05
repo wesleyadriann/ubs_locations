31653bdefd76f745a1ca8ef672bbad7dc8ad0f07
Wesley Adriann <wesleyadriann@gmail.com>
2022-04-05 12:01:59 -0300

    Adds port env
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const log = require('./log')
 const calculateDistance = require('./utils/calculateDistance')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ?? 3000
 app.use(express.json())
 
 
